ec7519a93a8bd1d487c9b4491f0e70a80a4ef3e2
Honza <rychnovsky.honza@gmail.com>
2023-09-21 09:18:30 +0200

    [#1236] Initial custom seed UI changes

--- demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/ui/screen/seed/view/ConfigureSeedView.kt
+++ demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/ui/screen/seed/view/ConfigureSeedView.kt
@@ -2,22 +2,38 @@ package cash.z.ecc.android.sdk.demoapp.ui.screen.seed.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import cash.z.ecc.android.sdk.WalletInitMode
 import cash.z.ecc.android.sdk.demoapp.R
+import cash.z.ecc.android.sdk.demoapp.util.fromResources
 import cash.z.ecc.android.sdk.fixture.WalletFixture
+import cash.z.ecc.android.sdk.model.BlockHeight
 import cash.z.ecc.android.sdk.model.PersistableWallet
 import cash.z.ecc.android.sdk.model.SeedPhrase
 import cash.z.ecc.android.sdk.model.ZcashNetwork
@@ -104,6 +120,50 @@ private fun ConfigureSeedMainContent(
         }
         Button(
             onClick = onNewWallet
+        ) {
+            Text(text = stringResource(id = R.string.seed_random))
+        }
+
+        Spacer(modifier = Modifier.height(36.dp))
+
+        Text(text = stringResource(R.string.seed_custom_title))
+
+        // TODO seed text field
+
+        val (height, setHeight) = rememberSaveable {
+            mutableStateOf("")
+        }
+
+        TextField(
+            value = height,
+            onValueChange = { heightString ->
+                val filteredHeightString = heightString.filter { it.isDigit() }
+                setHeight(filteredHeightString)
+            },
+            Modifier.fillMaxWidth(),
+            label = { Text(stringResource(id = R.string.seed_custom_birthday_height)) },
+            keyboardOptions = KeyboardOptions(
+                KeyboardCapitalization.None,
+                autoCorrect = false,
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Number
+            ),
+            keyboardActions = KeyboardActions(onAny = {}),
+            shape = RectangleShape,
+        )
+
+        val network = ZcashNetwork.fromResources(LocalContext.current)
+        Button(
+            onClick = {
+
+                val newWallet = PersistableWallet(
+                    zcashNetwork,
+                    BlockHeight.new(network, height.toLong()),
+                    SeedPhrase.new(WalletFixture.Ben.seedPhrase),
+                    WalletInitMode.RestoreWallet
+                )
+                onExistingWallet(newWallet)
+            }
         ) {
             Text(text = stringResource(id = R.string.seed_random))
         }
