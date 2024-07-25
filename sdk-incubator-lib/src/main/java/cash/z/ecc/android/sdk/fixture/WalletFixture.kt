63c8505d977206162c5b9df22620ef44cf055974
who-biz <37732338+who-biz@users.noreply.github.com>
2024-07-25 16:43:38 -0400

    Set birthday height to sapling activation for verus

--- sdk-incubator-lib/src/main/java/cash/z/ecc/android/sdk/fixture/WalletFixture.kt
+++ sdk-incubator-lib/src/main/java/cash/z/ecc/android/sdk/fixture/WalletFixture.kt
@@ -42,7 +42,7 @@ sealed class WalletFixture {
                     BlockHeight.new(zcashNetwork, 2170000L)
                 }
                 ZcashNetwork.ID_MAINNET -> {
-                    BlockHeight.new(zcashNetwork, 3000000L)
+                    BlockHeight.new(zcashNetwork, 227520L)
                 }
                 else -> error("Unknown network $zcashNetwork")
             }
@@ -88,7 +88,7 @@ sealed class WalletFixture {
                     BlockHeight.new(zcashNetwork, 2170000L)
                 }
                 ZcashNetwork.ID_MAINNET -> {
-                    BlockHeight.new(zcashNetwork, 3000000L)
+                    BlockHeight.new(zcashNetwork, 227520L)
                 }
                 else -> error("Unknown network $zcashNetwork")
             }
