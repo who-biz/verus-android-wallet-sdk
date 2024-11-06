dc0c4c9467741607a9803e26d587a894c1dafa27
who-biz <37732338+who-biz@users.noreply.github.com>
2024-11-06 16:22:01 -0500

    Modify Cargo, gradle files, and group name
    - To emulate packaging conventions I used on the newer SDK fork

--- buildSrc/src/main/java/cash/z/ecc/android/Deps.kt
+++ buildSrc/src/main/java/cash/z/ecc/android/Deps.kt
@@ -4,12 +4,12 @@ object Deps {
     // For use in the top-level build.gradle which gives an error when provided
     // `Deps.Kotlin.version` directly
     const val kotlinVersion =   "1.4.10"
-    const val group =           "cash.z.ecc.android"
+    const val group =           "com.github.who-biz"
     const val artifactName =    "zcash-android-sdk"
     const val versionName =     "1.2.1-beta04"
     const val versionCode =     1_02_01_204  // last digits are alpha(0XX) beta(2XX) rc(4XX) release(8XX). Ex: 1_08_04_401 is an release candidate build of version 1.8.4 and 1_08_04_800 would be the final release.
     const val description =     "This lightweight SDK connects Android to Zcash. It welds together Rust and Kotlin in a minimal way, allowing third-party Android apps to send and receive shielded transactions easily, securely and privately."
-    const val githubUrl =       "https://github.com/zcash/zcash-android-wallet-sdk"
+    const val githubUrl =       "https://github.com/who-biz/verus-android-wallet-sdk"
 
     // publishing
     //      NOTE: to upload run: ./gradlew bintrayUpload after setting BINTRAY_USER and BINTRAY_API_KEY as environment variable
