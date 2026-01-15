846e4fef9b2614832f554ef92e56cf5f840dc9cd
who-biz <37732338+who-biz@users.noreply.github.com>
2026-01-15 14:57:48 -0500

    Align ChannelKeys Kotlin class

--- model-lib/src/main/java/cash/z/ecc/android/sdk/model/ChannelKeys.kt
+++ model-lib/src/main/java/cash/z/ecc/android/sdk/model/ChannelKeys.kt
@@ -2,9 +2,7 @@ package cash.z.ecc.android.sdk.model
 
 data class ChannelKeys(
     val address: String,
-    val fvk: String,
-    val fvkHex: String,
-    val dfvkHex: String,
-    val ivk: String?,
-    val spendingKey: String?
+    val dfvkBytes: ByteArray,
+    val ivkBytes: ByteArray?,
+    val spendingKeyBytes: ByteArray?
 )
