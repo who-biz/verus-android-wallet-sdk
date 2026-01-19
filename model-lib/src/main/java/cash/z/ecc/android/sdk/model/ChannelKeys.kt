89cc13f45a0962f4a9a274dae28424a6bf3e5a73
who-biz <37732338+who-biz@users.noreply.github.com>
2026-01-19 10:59:38 -0500

    Remove now non-existent dfvkHex argument from ChannelKeys.kt

--- model-lib/src/main/java/cash/z/ecc/android/sdk/model/ChannelKeys.kt
+++ model-lib/src/main/java/cash/z/ecc/android/sdk/model/ChannelKeys.kt
@@ -3,7 +3,6 @@ package cash.z.ecc.android.sdk.model
 data class ChannelKeys(
     val address: String,
     val fvkBytes: ByteArray,
-    val dfvkBytes: ByteArray,
     val ivkBytes: ByteArray,
     val spendingKeyBytes: ByteArray?
 )
